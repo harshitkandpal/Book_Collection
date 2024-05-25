43c58ee179b9152834f02c6298c044254c9a632f
Harshit Kandpal <harshitheroh5@gmail.com>
2024-05-25 09:42:25 +0530

    basic responsiveness added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,21 +1,14 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 
 const Navbar = ({ role }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <div className="navbar">
       <div className="navbar-left">
         <Link to="/" className='navbar-brand'>Book Collection</Link>
       </div>
       <div className="navbar-right">
-        <div className={`navbar-links ${isOpen ? 'open' : ''}`}>
+        <div className="navbar-links">
           <Link to="/books" className='navbar-link'>Books</Link>
           {role === 'admin' && 
             <Link to="/addbook" className='navbar-link'>Add Books</Link>
@@ -25,11 +18,6 @@ const Navbar = ({ role }) => {
             : <Link to="/logout" className='navbar-link'>Logout</Link> 
           }
         </div>
-        <button className="hamburger-menu" onClick={toggleMenu}>
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-        </button>
       </div>
     </div>
   );
